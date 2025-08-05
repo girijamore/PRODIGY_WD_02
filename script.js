2cd43c21a3242f0a3b954df2ba24f03dbc127f73
girijamore <moregirija6@gmail.com>
2025-08-05 23:21:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let [seconds, minutes, hours] = [0, 0, 0];
+let display = document.getElementById("time");
+let timer = null;
+
+function updateTime() {
+  seconds++;
+  if (seconds === 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes === 60) {
+      minutes = 0;
+      hours++;
+    }
+  }
+
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+
+  display.innerText = `${h}:${m}:${s}`;
+}
+
+document.getElementById("start").onclick = () => {
+  if (timer !== null) return;
+  timer = setInterval(updateTime, 1000);
+};
+
+document.getElementById("pause").onclick = () => {
+  clearInterval(timer);
+  timer = null;
+};
+
+document.getElementById("reset").onclick = () => {
+  clearInterval(timer);
+  timer = null;
+  [seconds, minutes, hours] = [0, 0, 0];
+  display.innerText = "00:00:00";
+  document.getElementById("laps").innerHTML = "";
+};
+
+document.getElementById("lap").onclick = () => {
+  let lapTime = display.innerText;
+  let li = document.createElement("li");
+  li.innerText = `Lap: ${lapTime}`;
+  document.getElementById("laps").appendChild(li);
+};
